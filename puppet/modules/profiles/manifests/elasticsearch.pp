9d6e4df653267395222db9d6e0a0ce8159f0697a
Yulian Slobodyan <y.slobodian@gmail.com>
2015-08-11 01:54:31 -0500

    Initial profiles structure, draft linux profile

---
+++ puppet/modules/profiles/manifests/elasticsearch.pp
@@ -0,0 +1,7 @@
+#
+# Configures elasticsearch as a standalone or cluster node
+#
+
+class profiles::elasticsearch inherits profiles::linux {
+
+}
---
+++ puppet/modules/profiles/manifests/flume_agent.pp
@@ -0,0 +1,7 @@
+#
+# Installs and configures flume agent to receive log entries
+#
+
+class profiles::flume_agent inherits profiles::linux {
+
+}
