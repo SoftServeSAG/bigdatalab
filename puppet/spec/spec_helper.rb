6beaea618887e8619dda1f1567f2004a199c4dcc
Yulian Slobodyan <y.slobodian@gmail.com>
2015-08-18 13:43:13 +0300

    Set up spec in the root.

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -4,9 +4,13 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.module_path = File.join(Dir.pwd, 'modules')
+  puts "MODULE_PATH: #{c.module_path}"
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  puts "MANIFEST_DIR: #{c.manifest_dir}"
   c.environmentpath = File.join(Dir.pwd, 'spec')
+  puts "ENVIRONMENTPATH: #{c.environmentpath}"
   c.hiera_config = File.join(Dir.pwd, 'spec/hiera/hiera.yaml')
+  puts "HIERA_CONFIG: #{c.hiera_config}"
   c.default_facts = {
     :operatingsystem        => 'CentOS', 
     :operatingsystemrelease => '7.0', 
