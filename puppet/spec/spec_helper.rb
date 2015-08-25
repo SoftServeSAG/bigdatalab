6c23d212e2fd4d5fe0360474429c10b2d40a4458
Yulian Slobodyan <y.slobodian@gmail.com>
2015-08-25 15:14:16 +0300

    Fixing tests. Now should not fail under bundler

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require 'rspec-puppet'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+spec_path = File.expand_path(File.join(__FILE__, '..'))
+fixture_path = File.join(spec_path, '..', 'fixtures')
 
 RSpec.configure do |c|
-  c.module_path = File.join(Dir.pwd, 'modules')
+  c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.environmentpath = File.join(Dir.pwd, 'spec')
   c.hiera_config = File.join(Dir.pwd, 'spec/hiera/hiera.yaml')
