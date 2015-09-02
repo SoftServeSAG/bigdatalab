31ac6a433327c6d98b12d63bbf84b8479421f9ab
dsandu <dmytro.sandu@gmail.com>
2015-09-02 19:14:36 +0300

    Refactor profile, move static data to Hiera, add apache template. (reverted from commit 1eaaac48acc3c181408338f41f622783d238dd06)

--- puppet/local_modules/profiles/manifests/elasticsearch.pp
+++ puppet/local_modules/profiles/manifests/elasticsearch.pp
@@ -3,73 +3,115 @@
 #
 
 class profiles::elasticsearch (
-  $num_nodes    = 1,
-  $cluster_name = 'bdl-cluster',
-  $data_dir     = undef,
-  $aws_params   = undef
+  $num_nodes              = 1,
+  $cluster_name           = undef,
+  $data_dir               = '/var/lib/elasticsearch',
+  $aws_access_key         = undef,
+  $aws_secret_key         = undef,
+  $aws_region             = undef,
+  $aws_groups             = undef,
+  $aws_host_type          = undef,
+  $aws_ping_timeout       = undef,
+  $aws_availability_zones = undef
 ) {
-  require profiles::linux
+  include profiles::linux
   
-  $is_aws = ($::ec2_instance_id != undef)
+  #TODO use facter to detect AWS
+  $is_aws = true
+  #TODO use facter to detect RAM amount
+  $ram_amount_gb = 2
   
-  #Validation
-  validate_bool($is_aws)
-  validate_integer($num_nodes)
-  validate_string($cluster_name)
-  validate_string($data_dir)
-  if( $is_aws==true ){
-    validate_hash($aws_params)
-    validate_string($aws_params[access_key])
-    validate_string($aws_params[secret_key])
-    validate_string($aws_params[region])
-    validate_string($aws_params[host_type])
-    validate_integer($aws_params[ping_timeout_sec])
-  }
-  
-  $zen = {
-    'ping.multicast.enabled' => ( !$is_aws ),
-    'minimum_master_nodes'   => ( $num_nodes/2 + 1 )
-  }
-  
-  $cloud = undef
-  if( $is_aws ) {
-    $cloud = {
-      'aws' => {
-        'access_key' => $aws_params[access_key],
-        'secret_key' => $aws_params[secret_key],
-        'region'     => $aws_params[region]
-      }
-    }
-    $discovery = {
-      'type' => 'ec2',
-      'ec2'  => {
-        'groups'             => $::ec2_security_groups,
-        'host_type'          => $aws_params[host_type],
-        'ping_timeout'       => ($aws_params[ping_timeout_sec]+'s'),
-        'availability_zones' => $::ec2_placement_availability_zone
-      },
-      'zen'  => $zen
-    }
-  } else {
-    $discovery = {
-      'zen' => $zen
-    }
-  }
-  
-  $config = {
-    'cluster.name'           => $cluster_name,
-    'gateway.expected_nodes' => $num_nodes,
-    'cloud'                  => $cloud,
-    'discovery'              => $discovery
-  }
-  
-  $heap_size = $::memorysize_mb/2
-  
+  class {'wget':}
+  class {'jdk_oracle': } ->
   class { 'elasticsearch':
-    config        => $config,
+    package_url   => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.noarch.rpm',
+    config        => {
+      'cluster'    => {
+        'name'    => $cluster_name
+        'routing' => {
+          'allocation' => {
+            'cluster_concurrent_rebalance'      => 2,
+            'node_concurrent_recoveries'        => 2,
+            'node_initial_primaries_recoveries' => 4
+            'disk'                              => {
+              'threshold_enabled' => true
+              'watermark'         => {
+                'low'  => 0.85,
+                'high' => 0.9
+              }
+            }
+          }
+        }
+      },
+      'index'      => {
+        'number_of_replicas' => 1,
+        'number_of_shards'   => 1
+      },
+      'bootstrap'  => {
+        'mlockall' => true
+      },
+      'cloud'      => {
+        'aws' => {
+          'access_key' => $aws_access_key,
+          'secret_key' => $aws_secret_key ,
+          'region'     => $aws_region
+        }
+      },
+      'discovery'  => {
+        'type' => 'ec2', #aws only
+        'ec2'  => { #aws only
+          'groups'             => $aws_groups,
+          'host_type'          => $aws_host_type,
+          'ping_timeout'       => $aws_ping_timeout,
+          'availability_zones' => $aws_availability_zones
+        },
+        'zen'  => {
+          'ping'                 => {
+            'multicast' => {
+              'enabled' => ( !$is_aws )
+            }
+          },
+          #single-node cluster will not work if minimum_master_nodes > 1
+          'minimum_master_nodes' => ( $num_nodes/2 + 1 )
+        }
+      },
+      'gateway'    => {
+        'recover_after_time' => '5m',
+        'expected_nodes'     => $num_nodes,
+        'type'               => 'local' #TODO check if ok
+      },
+      'indices'    => {
+        'fielddata' => {
+          'cache' => {
+            'size' => '50%'
+          }
+        },
+        'cache'     => {
+          'filter' => {
+            'size' => '10%'
+          }
+        },
+        'recovery'  => {
+          'concurrent_streams' => 3,
+          'max_bytes_per_sec'  => '40mb'
+        }
+      },
+      'action'     => {
+        'destructive_requires_name' => false
+      },
+      'threadpool' => {
+        'search' => {
+          'queue_size' => 3000
+        }
+      }
+    },
+    datadir       => $data_dir
     init_defaults => {
-      'ES_HEAP_SIZE' => "#{heap_size}m",
-      'JAVA_HOME'    => $jdk_oracle::java_home
+      'ES_HEAP_SIZE'      => ( ($ram_amount_gb/2)+'g' ),
+      'MAX_OPEN_FILES'    => 65535,
+      'MAX_LOCKED_MEMORY' => 'infinity'
+      'JAVA_HOME'         => $jdk_oracle::java_home
     }
   }
+  
 }
