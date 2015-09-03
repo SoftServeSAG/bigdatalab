e133273301b846d84bd37c436f9fcb35b0a7754b
dsandu <dmytro.sandu@gmail.com>
2015-09-03 16:36:39 +0300

    Passing unit and acceptance tests

--- puppet/local_modules/profiles/manifests/elasticsearch.pp
+++ puppet/local_modules/profiles/manifests/elasticsearch.pp
@@ -2,116 +2,99 @@
 # Configures elasticsearch as a standalone or cluster node
 #
 
+# aws_* parameters are optional and needed only for AWS deployment
 class profiles::elasticsearch (
-  $num_nodes              = 1,
-  $cluster_name           = undef,
-  $data_dir               = '/var/lib/elasticsearch',
   $aws_access_key         = undef,
   $aws_secret_key         = undef,
   $aws_region             = undef,
   $aws_groups             = undef,
-  $aws_host_type          = undef,
-  $aws_ping_timeout       = undef,
-  $aws_availability_zones = undef
+  $aws_availability_zones = undef,
+  $aws_host_type          = 'private_ip',
+  $aws_ping_timeout_sec   = 30,
+  $num_nodes              = 1,
 ) {
   include profiles::linux
   
-  #TODO use facter to detect AWS
-  $is_aws = true
-  #TODO use facter to detect RAM amount
-  $ram_amount_gb = 2
+  $is_aws = ($::ec2_instance_id != undef)
+  $heap_size = ($::memorysize_mb/2).floor
+  $instance_name = 'es-01'
+
+  validate_integer($num_nodes, undef , 1)
   
-  class {'wget':}
-  class {'jdk_oracle': } ->
+  $min_master_nodes = ($num_nodes/2).floor + 1
+  
+  $config_common = {
+    'cluster.name'                                                 => 'bdl',
+    'cluster.routing.allocation.cluster_concurrent_rebalance'      => 2,
+    'cluster.routing.allocation.node_concurrent_recoveries'        => 2,
+    'cluster.routing.allocation.node_initial_primaries_recoveries' => 4,
+    'cluster.routing.allocation.disk.threshold_enabled'            => true,
+    'cluster.routing.allocation.disk.watermark.low'                => 0.85,
+    'cluster.routing.allocation.disk.watermark.high'               => 0.9,
+    'index.number_of_replicas'                                     => 1,
+    'index.number_of_shards'                                       => 1,
+    'bootstrap.mlockall'                                           => true,
+    'discovery.zen.minimum_master_nodes'                           => $min_master_nodes,
+    'gateway.recover_after_time'                                   => '5m',
+    'gateway.expected_nodes'                                       => $num_nodes,
+    'gateway.type'                                                 => 'local',
+    'indices.fielddata.cache.size'                                 => '50%',
+    'indices.cache.filter.size'                                    => '10%',
+    'indices.recovery.concurrent_streams'                          => 3,
+    'indices.recovery.max_bytes_per_sec'                           => '40mb',
+    'action.destructive_requires_name'                             => false,
+    'threadpool.search.queue_size'                                 => 3000
+  }
+  
+  if($is_aws) {
+    validate_string($aws_access_key)
+    validate_string($aws_secret_key)
+    validate_string($aws_region)
+    validate_string($aws_groups)
+    validate_string($aws_host_type)
+    validate_string($aws_availability_zones)
+    validate_integer($aws_ping_timeout_sec, undef, 1)
+    $config_cloud = {
+      'cloud.aws.access_key'                 => $aws_access_key,
+      'cloud.aws.secret_key'                 => $aws_secret_key ,
+      'cloud.aws.region'                     => $aws_region,
+      'discovery.type'                       => 'ec2',
+      'discovery.ec2.groups'                 => $aws_groups,
+      'discovery.ec2.host_type'              => $aws_host_type,
+      'discovery.ec2.ping_timeout'           => "${aws_ping_timeout_sec}s",
+      'discovery.ec2.availability_zones'     => $aws_availability_zones,
+      'discovery.zen.ping.multicast.enabled' => false,
+    }
+  } else {
+    $config_cloud = {
+      'discovery.zen.ping.multicast.enabled' => true,
+    }
+  }
+  
   class { 'elasticsearch':
     package_url   => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.noarch.rpm',
-    config        => {
-      'cluster'    => {
-        'name'    => $cluster_name
-        'routing' => {
-          'allocation' => {
-            'cluster_concurrent_rebalance'      => 2,
-            'node_concurrent_recoveries'        => 2,
-            'node_initial_primaries_recoveries' => 4
-            'disk'                              => {
-              'threshold_enabled' => true
-              'watermark'         => {
-                'low'  => 0.85,
-                'high' => 0.9
-              }
-            }
-          }
-        }
-      },
-      'index'      => {
-        'number_of_replicas' => 1,
-        'number_of_shards'   => 1
-      },
-      'bootstrap'  => {
-        'mlockall' => true
-      },
-      'cloud'      => {
-        'aws' => {
-          'access_key' => $aws_access_key,
-          'secret_key' => $aws_secret_key ,
-          'region'     => $aws_region
-        }
-      },
-      'discovery'  => {
-        'type' => 'ec2', #aws only
-        'ec2'  => { #aws only
-          'groups'             => $aws_groups,
-          'host_type'          => $aws_host_type,
-          'ping_timeout'       => $aws_ping_timeout,
-          'availability_zones' => $aws_availability_zones
-        },
-        'zen'  => {
-          'ping'                 => {
-            'multicast' => {
-              'enabled' => ( !$is_aws )
-            }
-          },
-          #single-node cluster will not work if minimum_master_nodes > 1
-          'minimum_master_nodes' => ( $num_nodes/2 + 1 )
-        }
-      },
-      'gateway'    => {
-        'recover_after_time' => '5m',
-        'expected_nodes'     => $num_nodes,
-        'type'               => 'local' #TODO check if ok
-      },
-      'indices'    => {
-        'fielddata' => {
-          'cache' => {
-            'size' => '50%'
-          }
-        },
-        'cache'     => {
-          'filter' => {
-            'size' => '10%'
-          }
-        },
-        'recovery'  => {
-          'concurrent_streams' => 3,
-          'max_bytes_per_sec'  => '40mb'
-        }
-      },
-      'action'     => {
-        'destructive_requires_name' => false
-      },
-      'threadpool' => {
-        'search' => {
-          'queue_size' => 3000
-        }
-      }
-    },
-    datadir       => $data_dir
+    config        => $config_common + $config_cloud,
+    datadir       => ['/var/lib/elasticsearch'],
     init_defaults => {
-      'ES_HEAP_SIZE'      => ( ($ram_amount_gb/2)+'g' ),
+      'ES_HEAP_SIZE'      => "${heap_size}m",
       'MAX_OPEN_FILES'    => 65535,
-      'MAX_LOCKED_MEMORY' => 'infinity'
-      'JAVA_HOME'         => $jdk_oracle::java_home
+      'MAX_LOCKED_MEMORY' => 'infinity',
+      'JAVA_HOME'         => '/usr/java/default'
     }
+  }
+  
+  elasticsearch::instance { $instance_name: }
+  elasticsearch::plugin{'mobz/elasticsearch-head':
+    module_dir => 'head',
+    instances  => $instance_name
+  }
+  elasticsearch::template { 'apache-logs':
+      file => 'puppet:///modules/profiles/templates/elasticsearch-template.json'
+  }
+
+  if($is_aws) {
+    elasticsearch::plugin { 'elasticsearch/elasticsearch-cloud-aws/2.5.1':
+      instances => $instance_name}
   }
   
 }
