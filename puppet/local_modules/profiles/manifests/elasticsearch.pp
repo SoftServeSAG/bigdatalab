dc5e78eccbb5a4e5d6aeb6ecc8880d18cb994739
dsandu <dmytro.sandu@gmail.com>
2015-10-02 16:09:10 +0300

    Make cluster name class parameter with no default and provide it via Hiera

--- puppet/local_modules/profiles/manifests/elasticsearch.pp
+++ puppet/local_modules/profiles/manifests/elasticsearch.pp
@@ -3,6 +3,8 @@
 #
 
 # aws_* parameters are optional and needed only for AWS deployment
+# force_aws_plugin(true) will install AWS plugin even if deployment target is
+#   not AWS. Possible usage scenario - S3 snapshot functionality
 class profiles::elasticsearch (
   $aws_access_key         = undef,
   $aws_secret_key         = undef,
@@ -11,7 +13,9 @@ class profiles::elasticsearch (
   $aws_availability_zones = undef,
   $aws_host_type          = 'private_ip',
   $aws_ping_timeout_sec   = 30,
+  $force_aws_plugin       = false,
   $num_nodes              = 1,
+  $cluster_name
 ) {
   include profiles::linux
   
@@ -19,31 +23,35 @@ class profiles::elasticsearch (
   $heap_size = ($::memorysize_mb/2).floor
   $instance_name = 'es-01'
 
+  validate_string($cluster_name)
   validate_integer($num_nodes, undef , 1)
   
   $min_master_nodes = ($num_nodes/2).floor + 1
   
   $config_common = {
-    'cluster.name'                                                 => 'bdl',
+    'cluster.name'                        => $cluster_name,
+    'index.number_of_replicas'            => 1,
+    'index.number_of_shards'              => 1,
+    'bootstrap.mlockall'                  => true,
+    'discovery.zen.minimum_master_nodes'  => $min_master_nodes,
+    'gateway.recover_after_time'          => '5m',
+    'gateway.expected_nodes'              => $num_nodes,
+    'gateway.type'                        => 'local',
+    'indices.fielddata.cache.size'        => '50%',
+    'indices.cache.filter.size'           => '10%',
+    'indices.recovery.concurrent_streams' => 3,
+    'indices.recovery.max_bytes_per_sec'  => '40mb',
+    'action.destructive_requires_name'    => false,
+    'threadpool.search.queue_size'        => 3000,
+  }
+  
+  $cluster_routing = {
     'cluster.routing.allocation.cluster_concurrent_rebalance'      => 2,
     'cluster.routing.allocation.node_concurrent_recoveries'        => 2,
     'cluster.routing.allocation.node_initial_primaries_recoveries' => 4,
     'cluster.routing.allocation.disk.threshold_enabled'            => true,
     'cluster.routing.allocation.disk.watermark.low'                => 0.85,
     'cluster.routing.allocation.disk.watermark.high'               => 0.9,
-    'index.number_of_replicas'                                     => 1,
-    'index.number_of_shards'                                       => 1,
-    'bootstrap.mlockall'                                           => true,
-    'discovery.zen.minimum_master_nodes'                           => $min_master_nodes,
-    'gateway.recover_after_time'                                   => '5m',
-    'gateway.expected_nodes'                                       => $num_nodes,
-    'gateway.type'                                                 => 'local',
-    'indices.fielddata.cache.size'                                 => '50%',
-    'indices.cache.filter.size'                                    => '10%',
-    'indices.recovery.concurrent_streams'                          => 3,
-    'indices.recovery.max_bytes_per_sec'                           => '40mb',
-    'action.destructive_requires_name'                             => false,
-    'threadpool.search.queue_size'                                 => 3000
   }
   
   if($is_aws) {
@@ -73,7 +81,7 @@ class profiles::elasticsearch (
   
   class { 'elasticsearch':
     package_url   => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.noarch.rpm',
-    config        => $config_common + $config_cloud,
+    config        => $config_common + $cluster_routing + $config_cloud,
     datadir       => ['/var/lib/elasticsearch'],
     init_defaults => {
       'ES_HEAP_SIZE'      => "${heap_size}m",
@@ -88,11 +96,16 @@ class profiles::elasticsearch (
     module_dir => 'head',
     instances  => $instance_name
   }
-  elasticsearch::template { 'apache-logs':
-      file => 'puppet:///modules/profiles/templates/elasticsearch-template.json'
+  elasticsearch::plugin{'elasticsearch/marvel/latest':
+    module_dir => 'marvel',
+    instances  => $instance_name
   }
+  
+#  elasticsearch::template { 'apache-logs':
+#      file => 'puppet:///modules/profiles/elasticsearch-template.json'
+#  }
 
-  if($is_aws) {
+  if($is_aws or $force_aws_plugin) {
     elasticsearch::plugin { 'elasticsearch/elasticsearch-cloud-aws/2.5.1':
       instances => $instance_name}
   }
