3eeb3c94a3e7f41edb837fc3b50cc028be0b77e4
bkomar <bkomar@softserveinc.com>
2015-11-12 19:43:18 +0200

    Fixed issues afret CR. Fixed Cluster deployment issues

--- puppet/local_modules/profiles/manifests/cloudera_director_client.pp
+++ puppet/local_modules/profiles/manifests/cloudera_director_client.pp
@@ -1,4 +1,5 @@
 class profiles::cloudera_director_client (
+    $deploy_cluster,
     $cluster_name,
     $instance_name_prefix,
     $private_key_path,
@@ -10,6 +11,7 @@ class profiles::cloudera_director_client (
   require profiles::common
 
   cloudera_director_client::deployment { $cluster_name :
+    deploy_cluster                 => $deploy_cluster,
     instance_name_prefix           => $instance_name_prefix,
     data_node_quantity             => $data_node_quantity,
     data_node_quantity_min_allowed => $data_node_quantity_min_allowed,
@@ -19,7 +21,6 @@ class profiles::cloudera_director_client (
     aws_security_group_ids         => $profiles::common::aws_security_group_id,
     aws_subnetId                   => $profiles::common::aws_subnetId,
     aws_ssh_username               => $profiles::common::aws_ssh_username,
-    aws_ami                        => $profiles::common::aws_ami,
     aws_ssh_private_key            => $private_key_path,
     aws_tag_env                    => $profiles::common::aws_tag_env,
     aws_tag_owner                  => $profiles::common::aws_tag_owner,
