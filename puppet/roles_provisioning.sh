908d00117f6658d1ff4238abcbf0a732fcf7f0dd
bkomar <bkomar@softserveinc.com>
2015-11-17 17:07:12 +0200

    SSSAG-678 - Refactor terraform deployment files (extract hardcoded es version) | SSSAG-682 - Obtain aws_instance.subnet_id in a proper way

--- puppet/roles_provisioning.sh
+++ puppet/roles_provisioning.sh
@@ -25,9 +25,6 @@ case "$PUPPET_ROLE" in
     sudo puppet apply --modulepath modules --hiera_config=hiera/hiera.yaml --yamldir hiera manifests/elasticsearch_kibana.pp
     ;;
 "cloudera_director_client")
-    awsSubnetId=$1
-    # Setup subnetID parameter as it's computed value and we are not able to prepare it in scope of 'null_dynamic_config'
-    echo "profiles::common::aws_subnetId: $awsSubnetId" >> hiera/hieradata/dynamic.yaml
     sudo puppet apply --modulepath modules --hiera_config=hiera/hiera.yaml --yamldir hiera manifests/cloudera_director_client.pp
     ;;
 "log_generator")
