d8e19ac33b8090324045f52d72b4d58ab0f496a8
bkomar <bkomar@softserveinc.com>
2015-11-17 18:42:18 +0200

    Fix small issue with left commented code

--- puppet/roles_provisioning.sh
+++ puppet/roles_provisioning.sh
@@ -3,7 +3,7 @@
 # Does preparation of an environment which is common for each puppet role
 prepareCommonEnv() {
     sudo yum -y install yum-plugin-fastestmirror
-    #sudo yum -y update
+    sudo yum -y update
     sudo yum -y remove java-*-openjdk
     sudo rpm -U https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
     sudo yum -y install puppet-agent
