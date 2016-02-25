fe1bf664fb4633ad95af2d4592e7b65f1b80025a
Ramin Gasanov <ramin.gas@gmail.com>
2016-02-25 17:19:51 +0200

    Added install of Python modules

--- core-install.sh
+++ core-install.sh
@@ -11,7 +11,9 @@ if [[ $(ls /etc/*release 2>/dev/null | grep 'redhat' 2>/dev/null) != '' ]]; then
   yum -y groupinstall "Development tools"
   yum -y install openssl-devel readline-devel zlib-devel
   yum -y install git unzip wget
+  yum -y install python-pip
   yum -y install https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.rpm
+  pip install -r requirements.txt
   if [[ $(cat /etc/redhat-release | grep ' 7') != '' ]]; then
     yum -y install http://download.virtualbox.org/virtualbox/5.0.8/VirtualBox-5.0-5.0.8_103449_el7-1.x86_64.rpm
   elif [[ $(cat /etc/redhat-release | grep ' 6') != '' ]]; then
